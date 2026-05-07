95e26b8661200454dda074cfac129a1e2d98080d
Wnsyi <255818502@qq.com>
2026-05-08 06:08:41 +0800

    1.1.0版本

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.caiberclick.hospital',
-  appName: '赛博华佗',
+  appName: '开药吗',
   webDir: 'dist',
   server: {
     url: 'https://game-one-d1gx1gwhbee34fff7-1428019926.tcloudbaseapp.com/mental-hospital/',
